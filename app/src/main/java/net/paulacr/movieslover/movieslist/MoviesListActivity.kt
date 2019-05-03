e1b2b722d23da08a177f992f272c70b51ef1e605
Paula Rosa <paula.mecatronica@gmail.com>
2019-05-03 17:27:11 -0300

    Apply injection using koin

--- app/src/main/java/net/paulacr/movieslover/movieslist/MoviesListActivity.kt
+++ app/src/main/java/net/paulacr/movieslover/movieslist/MoviesListActivity.kt
@@ -1,2 +1,17 @@
 package net.paulacr.movieslover.movieslist
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import net.paulacr.movieslover.R
+import org.koin.android.viewmodel.ext.android.viewModel
+
+class MoviesListActivity : AppCompatActivity() {
+
+    private val viewModel : MoviesListViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
--- app/src/main/java/net/paulacr/movieslover/movieslist/MoviesListViewModel.kt
+++ app/src/main/java/net/paulacr/movieslover/movieslist/MoviesListViewModel.kt
@@ -1,8 +1,9 @@
 package net.paulacr.movieslover.movieslist
 
 import android.app.Application
-import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LifecycleObserver
 
-class MoviesListViewModel(app: Application) : ViewModel() {
+class MoviesListViewModel(app: Application) : AndroidViewModel(app), LifecycleObserver {
 
 }
